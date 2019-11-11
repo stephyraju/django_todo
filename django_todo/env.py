6a8b41192608d471f77e1196ef8930767648a19a
Stephy Raju <stephyraju@Stephys-MacBook-Air.local>
2019-11-11 10:37:30 +0000

    secret key updated

---
+++ django_todo/env.py
@@ -0,0 +1,3 @@
+import os
+
+os.environ.setdefault("DJANGO_SECRET_KEY", "o!&xoaankd_u6jgqub+rugas%rkmb@b^!rug_p19d2+mll@swo")
--- django_todo/settings.py
+++ django_todo/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 import dj_database_url
+import env
+
+if os.environ.get('DEVELOPMENT'):
+    development = True
+else:
+    development = False
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -21,14 +27,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4g0n#3@(vscop2&sf70(!#8_ge6dp21n&tyye@=#p9^o2ump@t'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', "Env Value Not Loaded")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = development
 
 #A LLOWED_HOSTS = ["0.0.0.0","django-todo-sr.herokuapp.com"]
 ALLOWED_HOSTS = [os.environ.get('C2_HOSTNAME'), os.environ.get('HOSTNAME')]
-
+ALLOWED_HOSTS = ["0.0.0.0"]
 
 # Application definition
 
@@ -75,14 +81,15 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if development:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+     }
     }
-}
-DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
+else:
+    DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
 # "postgres://mbmmwmpyppkvfz:1422e064e099c6921fb3dcae99708c9b2454c79cfe9a3c32b8705aa17987d944@ec2-54-247-72-30.eu-west-1.compute.amazonaws.com:5432/d7aum4vifdo7tq"
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
